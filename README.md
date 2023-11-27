feb4c30f3733388712a45d53a2785ec66fbda143
mystery000 <lkostyrka17@gmail.com>
2023-11-27 14:53:24 -0500

    Get all category product links

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Sainsburys
+
 This is a trusted web scraper for extracting products from Sainburys.
+
+# How to setup
+
+## Ubuntu
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install requests
+pip install html5lib
+pip install beautifulsoup4
+pip install selenium
+pip install pandas
+
+## Windows
+
+python -m venv venv
+venv/Scripts/activate
+
+pip install requests
+pip install html5lib
+pip install beautifulsoup4
+pip install selenium
+pip install pandas
+
+# How to run
+
+## Ubuntu
+
+source venv/bin/activate
+python3 main.py
+
+## Windows
+
+venv/Scripts/activate
+python main.py
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+import os
 import sys
+import csv
 import json
 import logging
 import requests
+import pandas as pd
+import logging.handlers
+from typing import List
+import multiprocessing as mp
 from bs4 import BeautifulSoup
-from typing import List, Dict
 from selenium.webdriver import Remote, ChromeOptions
-from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
+from urllib.parse import urlparse, parse_qs, urlunparse
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 
 BASE_URL = "https://www.sainsburys.co.uk/shop"
-AUTH = 'brd-customer-hl_4d0a14cd-zone-sainsburys:jx4uczrgz9p2'
+AUTH = 'brd-customer-hl_6c86c9b0-zone-scraping_browser:ynhgmc04x4i5'
 SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
 
 def get_categories() -> List[str]:
@@ -17,61 +22,97 @@ def get_categories() -> List[str]:
     response = requests.get(url)
     content = json.loads(response.content)
     categories = (f"{BASE_URL}/{category['s']}/seeall?fromMegaNav=1" for category in content["category_hierarchy"]["c"])    
-    return categories
+    return list(categories)
 
-def get_category_products(connection: ChromiumRemoteConnection, category_url: str) -> List[str]:
-    print(f"Category: {category_url}")
-    with Remote(connection, options=ChromeOptions()) as driver:
+class CategoryScraper():
+    _queue: mp.Queue
+    _sbr_connection: ChromiumRemoteConnection
+    _categories: str
+
+    def __init__(self, queue: mp.Queue, sbr_connection: ChromiumRemoteConnection, categories: List[str]) -> None:
+        self._queue = queue
+        self._sbr_connection = sbr_connection
+        self._categories = categories
+
+    def get_category_products(self, category_url: str) -> List[str]:
+        _products: List[str] = []
+
+        print(f"Category: {category_url}\n")
+
+        try:
+            with Remote(self._sbr_connection, options=ChromeOptions()) as driver:
+                driver.get(category_url)
+
+                html = driver.page_source
+
+                page = BeautifulSoup(html, 'html5lib')
+                
+                pagination = page.find('div', class_='pagination')
+
+                if pagination:
+                    # Get the total page number
+                    next = pagination.find('li', class_='next')
+                    previous = next.find_previous_sibling('li')
+                    last_page_number = int(previous.find_all('span')[1].text)
+                    parsed_url = urlparse(previous.a['href'])
+                    query_params = parse_qs(parsed_url.query, keep_blank_values=True)
+
+                    # Iterate pagination
+                    for page_number in range(0, last_page_number):
+                        query_params['beginIndex'] = [str(60 * page_number)]
+                        modified_query = '&'.join([f"{k}={v[0]}" for k, v in query_params.items()])
+                        current_page_url = urlunparse(parsed_url._replace(query=modified_query))
+
+                        print(f"Scraping page content[{os.getpid()}]: {current_page_url}")
+
+                        response = requests.get(current_page_url)
+                        current_page = BeautifulSoup(response.content, 'html5lib')
+
+                        if current_page.html.find('div', class_='pagination'):
+                            for product in current_page.html.findAll('div', class_='product'):
+                                try:
+                                    url = product.find('div', class_='productInfo').a['href']
+                                except TypeError:
+                                    continue
+
+                                if url: 
+                                    _products.append(url)
+
+        except Exception as e:
+            print(f'Exception: {str(e)}')
         
-        products: List[str] = []
-
-        driver.get(category_url)
-
-        # logging.info('Taking page screenshot to file page.png')
-        # driver.get_screenshot_as_file('./page.png')
-        # logging.info('Navigated! Scraping page content...')
-
-        html = driver.page_source
-
-        page = BeautifulSoup(html, 'html5lib')
+        finally :
+            return _products
+    
+    def run(self) -> List[str]:
+        for category in self._categories:
+            category_products = self.get_category_products(category)
+            csv_file_name = "product_detail_links.csv"
+            start_index = 1
+            try:
+                if os.path.exists(csv_file_name):
+                    df = pd.read_csv(csv_file_name)
+                    start_index = df['Id'].iloc[-1] + 1  # Get the last index and increment by 1
+            except pd.errors.EmptyDataError:
+                pass
         
-        pagination = page.find('div', attrs={'class': 'pagination'})
-
-        if pagination:
-            # Get the total page number
-            next = pagination.find('li', class_='next')
-            previous = next.find_previous_sibling('li')
-            last_page_number = int(previous.find_all('span')[1].text)
-            parsed_url = urlparse(previous.a['href'])
-            query_params = parse_qs(parsed_url.query, keep_blank_values=True)
-
-            # Iterate pagination
-            for page_number in range(0, last_page_number):
-                query_params['beginIndex'] = [str(60 * page_number)]
-                modified_query = '&'.join([f"{k}={v[0]}" for k, v in query_params.items()])
-                current_page_url = urlunparse(parsed_url._replace(query=modified_query))
-
-                print(f"Scraping page content: {current_page_url}")
-                driver.get(current_page_url)
-                current_page = BeautifulSoup(driver.page_source, 'html5lib')
-                if current_page.find('div', attrs={'class': 'pagination'}):
-                    for product in current_page.findAll('div', attrs={'class': 'product'}):
-                        try:
-                            url = product.a['href']
-                        except TypeError:
-                            continue
-
-                        if url: 
-                            products.append(url)
-
-        return products
+            with open(csv_file_name, 'a', newline='') as csv_file:
+                fieldnames = ['Id', 'Link']
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                if csv_file.tell() == 0:
+                    writer.writeheader()
+                for i, product in enumerate(category_products, start=start_index):
+                    writer.writerow({'Id': i, 'Link': product})
 
 def main(log_to_file: bool = False):
+    csv_file_name = "product_detail_links.csv"
+    if os.path.exists(csv_file_name):
+        os.remove(csv_file_name)
+
+    queue = mp.Queue(maxsize=100)
+
     try:
         print("Starting...")
-
-        products: List[str] = []
-
         if log_to_file:
             logging.basicConfig(
                 format="[%(asctime)s] %(message)s",
@@ -90,28 +131,29 @@ def main(log_to_file: bool = False):
                 handlers=[logging.StreamHandler(sys.stdout)]
             )
 
-        try:
-            logging.info('Connecting to Scraping Browser...')
-            sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
-            logging.info('Connected!')
-        except Exception as ex:
-            logging.error("Scraping Browser connection failed due to ", str(ex))
-
         categories = get_categories()
-        for category in list(categories)[:3]:
-            category_products = get_category_products(sbr_connection, category)
-            products += category_products
-            print(len(category_products))
 
-        for product in products:
-            print(product)
+        try:
+            print(f'Connecting to Scraping Browser: {SBR_WEBDRIVER} ...')
+            sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+            print('Connected!')
+            processes = [
+                mp.Process(target=CategoryScraper(queue, sbr_connection, categories[:6]).run), 
+                mp.Process(target=CategoryScraper(queue, sbr_connection, categories[6:12]).run),
+                mp.Process(target=CategoryScraper(queue, sbr_connection, categories[12:18]).run),
+                ]
+            for process in processes: process.start()
+            for process in processes: process.join()
 
-        print("Finished!")
+        except Exception as e:
+            logging.error(f"Scraping Browser connection failed due to {str(e)}")
+
     except KeyboardInterrupt:
-        logging.info("Quitting...")
-    except Exception as ex:
-        logging.warning("Exception")
-        logging.warning(str(ex))
+        print("Quitting...")
+    except Exception as e:
+        logging.warning(f"Exception: {str(e)}")
+
+    print("Finished!")
 
 if (__name__ == '__main__'):
     main()
